f9e6a70fe9fb932c3329ce3bacb8f32c042f0bca
Jack Sparrow <nicolasmeireles1004@gmail.com>
2025-02-01 19:42:09 -0300

    feat(product-service): add BeanConfig and ProductController
    - Added BeanConfig to configure Spring beans for the product-service.
    - Implemented ProductController to handle HTTP requests for product operations.
    - Created POST endpoint `/products` to handle product creation.
    - Defined createProductUseCase bean to encapsulate business logic for product creation.

--- product-service/src/main/java/com/teste/product_service/infra/config/BeanConfig.java
+++ product-service/src/main/java/com/teste/product_service/infra/config/BeanConfig.java
@@ -1,7 +1,16 @@
 package com.teste.product_service.infra.config;
 
+import com.teste.product_service.core.gateway.ProductGateway;
+import com.teste.product_service.core.usecase.CreateProductUseCase;
+import com.teste.product_service.core.usecase.CreateProductUseCaseImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
+
+    @Bean
+    public CreateProductUseCase createProductUseCase(ProductGateway productGateway) {
+        return new CreateProductUseCaseImpl(productGateway);
+    }
 }
--- product-service/src/main/java/com/teste/product_service/infra/controllers/ProductController.java
+++ product-service/src/main/java/com/teste/product_service/infra/controllers/ProductController.java
@@ -1,15 +1,32 @@
 package com.teste.product_service.infra.controllers;
 
+import com.teste.product_service.core.domain.entities.Product;
+import com.teste.product_service.core.usecase.CreateProductUseCase;
+import com.teste.product_service.infra.dtos.ProductRequestDto;
+import com.teste.product_service.infra.dtos.ProductResponseDto;
+import com.teste.product_service.infra.mapper.ProductDtoMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
+
+    private final CreateProductUseCase createProductUseCase;
+
+    public ProductController(CreateProductUseCase createProductUseCase) {
+        this.createProductUseCase = createProductUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto productRequestDto) {
+        Product product = createProductUseCase.execute(ProductDtoMapper.toDomain(productRequestDto));
+        URI locationProduct = URI.create(String.format("/api/v1/product/%s", product.id()));
+        return ResponseEntity.created(locationProduct).body(ProductDtoMapper.toDtoResponse(product));
+    }
 
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> testProductService() {
