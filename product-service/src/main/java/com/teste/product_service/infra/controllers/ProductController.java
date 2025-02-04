07e75d4e76b0b950b5edd42eddcd398d97fcde5a
Jack Sparrow <nicolasmeireles1004@gmail.com>
2025-02-04 00:25:51 -0300

    feat(product-service): implement log in controllers

--- product-service/src/main/java/com/teste/product_service/infra/controllers/ProductController.java
+++ product-service/src/main/java/com/teste/product_service/infra/controllers/ProductController.java
@@ -7,6 +7,8 @@ import com.teste.product_service.core.usecase.FindProductByIdUseCase;
 import com.teste.product_service.infra.dtos.ProductRequestDto;
 import com.teste.product_service.infra.dtos.ProductResponseDto;
 import com.teste.product_service.infra.mapper.ProductDtoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     private final CreateProductUseCase createProductUseCase;
     private final FindProductByIdUseCase findProductByIdUseCase;
@@ -33,24 +37,28 @@ public class ProductController {
     public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto productRequestDto) {
         Product product = createProductUseCase.execute(ProductDtoMapper.toDomain(productRequestDto));
         URI locationProduct = URI.create(String.format("/api/v1/product/%s", product.id()));
+        log.info("Request received - Endpoint: POST /api/v1/product, Payload: {}", product);
         return ResponseEntity.created(locationProduct).body(ProductDtoMapper.toDtoResponse(product));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDto> findProductById(@PathVariable String id) {
         Product product = findProductByIdUseCase.execute(id);
+        log.info("Request received - Endpoint: GET /api/v1/product/{}", id);
         return ResponseEntity.ok(ProductDtoMapper.toDtoResponse(product));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ProductResponseDto> deactiveProduct(@PathVariable String id) {
         Product product = deactiveProductUseCase.execute(id);
+        log.info("Request received - Endpoint: DELETE /api/v1/product/{}", id);
         return ResponseEntity.ok(ProductDtoMapper.toDtoResponse(product));
     }
 
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> testProductService() {
         Map<String, String> response = Map.of("message", "Product service is working.");
+        log.info("Request received - Endpoint: GET /api/v1/product/test");
         return ResponseEntity.ok(response);
     }
 }
