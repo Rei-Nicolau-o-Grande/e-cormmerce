34f0312b806ee53bded35a338afcf53f713f6ab4
Jack Sparrow <nicolasmeireles1004@gmail.com>
2025-02-04 00:26:56 -0300

    feat(order-service): implement log in controllers

--- order-service/src/main/java/com/teste/order_service/infra/controllers/OrderController.java
+++ order-service/src/main/java/com/teste/order_service/infra/controllers/OrderController.java
@@ -5,6 +5,8 @@ import com.teste.order_service.core.usecase.CreateOrderUseCase;
 import com.teste.order_service.infra.dtos.OrderRequestDto;
 import com.teste.order_service.infra.dtos.OrderResponseDto;
 import com.teste.order_service.infra.mapper.OrderDtoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     private final CreateOrderUseCase createOrderUseCase;
 
@@ -25,12 +29,14 @@ public class OrderController {
     public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderRequestDto requestDto) {
         Order order = createOrderUseCase.execute(OrderDtoMapper.toDomain(requestDto));
         URI location = URI.create(String.format("/api/v1/order/%s", order.id()));
+        log.info("Request received - Endpoint: POST /api/v1/order, Payload: {}", order);
         return ResponseEntity.created(location).body(OrderDtoMapper.toDtoResponse(order));
     }
 
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> testOrderService() {
         Map<String, String> response = Map.of("message", "Order service is working.");
+        log.info("Request received - Endpoint: GET /api/v1/order/test");
         return ResponseEntity.ok(response);
     }
 }
